3ffc63557f32a5c8097e4fb58ed34ca3dadd9845
ihminq <hminh250104@gmail.com>
2025-06-28 01:00:18 +0700

    Add a script to control UI Interactions

---
+++ AstroDuo/Assets/Scripts/UIControl.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIControl : MonoBehaviour
+{
+    [Header("Scale Settings")]
+    [SerializeField] 
+    private Vector3 hoverScale = new Vector3(1.2f, 1.2f, 1.2f); // Image scale 1.2x when hover
+
+    public void ToSetting()
+    {
+        SceneManager.LoadScene(GameConst.SETTING_SCENE);
+    }
+
+    public void ToSelectSkin()
+    {
+        SceneManager.LoadScene(GameConst.SELECT_SKIN_SCENE);
+    }
+
+    public void OnButtonHoverEnter(RectTransform targetRectTransform)
+    {
+        if (targetRectTransform != null)
+        {
+            targetRectTransform.localScale = hoverScale;
+        }
+    }
+
+    public void OnButtonHoverExit(RectTransform targetRectTransform)
+    {
+        if (targetRectTransform != null)
+        {
+            targetRectTransform.localScale = Vector3.one; // return default scale
+        }
+    }
+}
