dda7f0a341b071263b1f0c4622dcab36277ee8ac
ihminq <hminh250104@gmail.com>
2025-06-30 21:28:21 +0700

    Add more const value

--- AstroDuo/Assets/Scripts/Const/GameConst.cs
+++ AstroDuo/Assets/Scripts/Const/GameConst.cs
@@ -4,5 +4,6 @@ public static class GameConst
 {
     public const int MAX_SCORE = 5;
     public const string SETTING_SCENE = "SettingScene";
+    public const string MAIN_SCENE = "MainScene";
     public const string SELECT_SKIN_SCENE = "SelectSkinScene";
 }
