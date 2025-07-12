75f73d5f452e990537f214c897ed45143ddc512b
ihminq <hminh250104@gmail.com>
2025-07-12 19:10:59 +0700

    Add new scene constants for score and maps

--- AstroDuo/Assets/Scripts/Const/GameConst.cs
+++ AstroDuo/Assets/Scripts/Const/GameConst.cs
@@ -7,4 +7,10 @@ public static class GameConst
     public const string MAIN_SCENE = "MainScene";
     public const string SELECT_SKIN_SCENE = "SelectSkinScene";
     public const string SAMPLE_SKIN_SCENE = "SampleScene";
+    public const string SCORE_SCENE = "ScoreScene";
+    public const string MAP_1 = "Map_1";
+    public const string MAP_2 = "Map_1";
+    public const string MAP_4 = "Map_4";
+    public const string MAP_5 = "Map_5";
+    public const string MAP_6 = "Map_6";
 }
